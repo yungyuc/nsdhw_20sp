273f37af8c07157f685f36564c09f68ad2dee33a
Alan Desktop <csvt32745@hotmail.com>
2020-03-17 18:18:58 +0800

    Detect python version and switch exec

--- hw1/csvt32745/countline.py
+++ hw1/csvt32745/countline.py
@@ -1,8 +1,32 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import sys
-import os.path
+import os
+import subprocess
 
+pyEnv = os.environ.get('PYTHON_BIN', " ")
+
+if pyEnv == "python2":
+    # execute python2
+    if sys.version_info.major == 2:
+        pass
+    else:
+        #print("{0} to {1}".format(sys.version_info.major, 2))
+        subprocess.call(["python2", sys.argv[0]])
+        exit(0)
+elif pyEnv == "python3":
+    # execute python3
+    if sys.version_info.major == 3:
+        pass
+    else:
+        #print("{0} to {1}".format(sys.version_info.major, 3))
+        subprocess.call(["python3"] + sys.argv[0:2])
+        exit(0)
+else:
+    sys.stdout.write("exec: {0}: not found\n".format(pyEnv))
+    exit(1)
+    
+# Main Procedural
 
 if len(sys.argv) < 2:
     sys.stdout.write('missing file name\n')
