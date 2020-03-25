84f0ba8dd6383386082533972c85aa066be9370e
Alan Desktop <csvt32745@hotmail.com>
2020-03-25 21:26:27 +0800

    Init

---
+++ hw2/csvt32745/q1/line.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class Line
+{
+public:
+    Line();
+    Line(Line const & );
+    Line(Line       &&);
+    Line & operator=(Line const & );
+    Line & operator=(Line       &&);
+    Line(size_t size);
+    ~Line();
+    size_t size() const;
+    float & x(size_t it) const;
+    float & x(size_t it);
+    float & y(size_t it) const;
+    float & y(size_t it);
+private:
+    // Member data.
+}; /* end class Line */
+
+// Testcase
+int main(int, char **)
+{
+    Line line(3);
+    line.x(0) = 0; line.y(0) = 1;
+    line.x(1) = 1; line.y(1) = 3;
+    line.x(2) = 2; line.y(2) = 5;
+
+    Line line2(line);
+    line2.x(0) = 9;
+
+    std::cout << "line: number of points = " << line.size() << std::endl;
+    for (size_t it=0; it<line.size(); ++it)
+    {
+        std::cout << "point " << it << ":"
+                  << " x = " << line.x(it)
+                  << " y = " << line.y(it) << std::endl;
+    }
+
+    std::cout << "line2: number of points = " << line.size() << std::endl;
+    for (size_t it=0; it<line.size(); ++it)
+    {
+        std::cout << "point " << it << ":"
+                  << " x = " << line2.x(it)
+                  << " y = " << line2.y(it) << std::endl;
+    }
+
+    return 0;
+}
