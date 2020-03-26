bc85ebdb31f38107f49e66ca1c7dc80bf57353c6
Alan Desktop <csvt32745@hotmail.com>
2020-03-26 14:32:42 +0800

    Implement python unit test; Makefile

--- hw2/csvt32745/q2/_vector.cpp
+++ hw2/csvt32745/q2/_vector.cpp
@@ -7,24 +7,18 @@
 
 namespace py = pybind11;
 
-float radian_between_2dvecs(std::vector<float> vecA, std::vector<float> vecB)
+float radian_between_2dvecs(std::array<float, 2> vecA, std::array<float, 2> vecB)
 {
-    if(vecA.size() != 2 || vecB.size() != 2){
-        std::cerr << "Error: vector lengh" << std::endl;
-        return HUGE_VALF;
-    }
-    
     float a = vecA[0]*vecA[0] + vecA[1]*vecA[1];
     float b = vecB[0]*vecB[0] + vecB[1]*vecB[1];
     if(a < FLT_MIN || b < FLT_MIN)
         return 0.0;
 
-    float d = (vecA[0]*vecB[0] + vecA[1]*vecB[1])/sqrtf(a*b);
+    float d = (vecA[0]*vecB[0] + vecA[1]*vecB[1])/sqrt(a*b);
 
-    return acosf(d);
+    return acos(d);
 }
 
 PYBIND11_MODULE(_vector, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
     m.def("radian_between_2dvecs", &radian_between_2dvecs, "Calculate radians between 2 vectors with 2-dim");
 }
