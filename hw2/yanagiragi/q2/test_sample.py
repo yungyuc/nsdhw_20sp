d5560b9c234e64a73859e6f7ee41f75fce853adf
yanagiragi <qaz90473@gmail.com>
2020-04-07 18:19:44 +0800

    update test for action ci

--- hw2/yanagiragi/q2/test_sample.py
+++ hw2/yanagiragi/q2/test_sample.py
@@ -3,13 +3,23 @@ import _vector
 import numpy as np
 import math
 
+def run_test(first, second, case):
+    if np.linalg.norm(first) * np.linalg.norm(second) == 0:
+        ans = 0
+    else:
+        ans = math.acos( np.dot(first, second) / (np.linalg.norm(first) * np.linalg.norm(second)) )
+    calc = _vector.calc(first, second)
+    # print(calc, ans)
+    if case == 1:
+        assert math.isnan(calc)
+    else:
+        assert abs(calc - ans) < 1e-5
+
 def test_answer():
-    first = [1,2]
-    second = [3,4]
-    ans = math.acos( np.dot(first, second) / (np.linalg.norm(first) * np.linalg.norm(second)) )
-    err = abs(_vector.calc(first, second) - ans)
-    print('Error = {}'.format(err))
-    assert abs(err) < 1e-9
+    run_test([0,0], [0,0], 1)
+    run_test([1,2], [1,2], 2)
+    run_test([1,0], [0,4], 3)
+    run_test([1,2], [3,4], 4)
 
 if __name__ == "__main__":
     test_answer()
