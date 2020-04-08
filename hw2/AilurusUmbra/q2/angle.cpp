c997f42453de8f41ced989fc1ed3d1b490c1e638
AilurusUmbra <c2525441.cs04@g2.nctu.edu.tw>
2020-04-08 16:20:56 +0800

    Finish q2

---
+++ hw2/AilurusUmbra/q2/angle.cpp
@@ -0,0 +1,50 @@
+#include <pybind11/pybind11.h>
+#include <vector>
+#include <cmath>
+
+// These 2 constants could be found in float.h
+#define FLT_MIN 1.175494351e-38F
+#define FLT_EPSILON 1.192092896e-07F
+
+namespace py = pybind11;
+
+class Point{
+  public:
+
+    Point(float px, float py) { coord = {px, py}; }
+    std::array<float, 2> coord;
+    const float& operator[] (int index) const { return coord[index]; };
+};
+
+
+float get_angle(const Point& p1, const Point& p2){
+
+    float len1 = p1[0] * p1[0] + p1[1] * p1[1];
+    float len2 = p2[0] * p2[0] + p2[1] * p2[1];
+    
+    // avoid 0 division  
+    if (len1 < FLT_MIN + FLT_EPSILON || 
+        len2 < FLT_MIN + FLT_EPSILON)
+      return 0.0;
+
+    float dot = p1[0] * p2[0] + p1[1] * p2[1];
+
+    float a = dot / sqrt(len1 * len2);
+
+    if(a >= 1.0)
+        return 0.0;
+    else if(a <= -1.0)
+        return M_PI;
+    else
+        return acos(a);
+}
+
+PYBIND11_MODULE(angle, m) {
+  
+  py::class_<Point>(m, "_vector")
+        .def(py::init<float, float>());
+
+  m.def("get_angle",
+        &get_angle, 
+        "A function which calculate angle between two points." );
+}
