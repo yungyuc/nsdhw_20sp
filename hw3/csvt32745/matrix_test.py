e042c522d9326f2beca61c6c9023f9b9ad910433
Alan Desktop <csvt32745@hotmail.com>
2020-04-23 20:42:35 +0800

    Add performance.txt, comment the time-test block in pytest

--- hw3/csvt32745/matrix_test.py
+++ hw3/csvt32745/matrix_test.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 import numpy.random as rd
 import math
+import time
 from _matrix import Matrix, multiply_naive, multiply_mkl
 
 class matrix_test(unittest.TestCase):
@@ -15,6 +16,37 @@ class matrix_test(unittest.TestCase):
         a = Matrix(rd.random((self.m, self.k)))
         b = Matrix(rd.random((self.k, self.n)))
         self.assertTrue(np.allclose(multiply_naive(a, b), multiply_mkl(a,b)))
+
+    # def testTime(self):
+    #     m = 1234
+    #     n = 1234
+    #     k = 1234
+    #     a = Matrix(rd.random((m, k)))
+    #     b = Matrix(rd.random((k, n)))
+
+    #     timer_a = []
+    #     timer_b = []
+    #     for i in range(5):
+    #         timer = time.time()
+    #         ans_nav = multiply_naive(a, b)
+    #         timer_a.append(time.time() - timer)
+
+    #         timer = time.time()
+    #         ans_mkl = multiply_mkl(a,b)
+    #         timer_b.append(time.time() - timer)
+    #         self.assertTrue(np.allclose(ans_nav, ans_mkl))
+        
+    #     with open("performance.txt", "w") as f:
+    #         f.write("Average time measured for 5 times\n")
+    #         f.write("size: m={} k={} n={}\n".format(m, k, n))
+    #         f.write("multiply_naive: {} secs\n".format("%.4f"%np.average(timer_a)))
+    #         f.write("multiply_mkl: {} secs\n".format("%.4f"%np.average(timer_b)))
+    #         f.close()
+
+        
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
