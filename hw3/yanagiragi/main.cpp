ab51131d3b4b88cc6dc9b38f45ff8a65ed3f4298
yanagiragi <qaz90473@gmail.com>
2020-04-24 16:10:42 +0800

    init hw3

---
+++ hw3/yanagiragi/main.cpp
@@ -0,0 +1,39 @@
+#include "matrix.hpp"
+
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+    Matrix mat1(2, 3, std::vector<double>{1, 2, 3, 4, 5, 6});
+    Matrix mat2(3, 2, std::vector<double>{1, 2, 3, 4, 5, 6});
+
+    // version from yungyuc
+    Matrix mat3 = mat1 * mat2;
+
+    // version from yanagi
+    Matrix mat3_naive = Matrix::multiply_naive(mat1, mat2);
+    Matrix mat3_mkl = Matrix::multiply_mkl(mat1, mat2);
+
+    std::cout << ">>> A(2x3) times B(3x2):" << std::endl;    
+    std::cout << "matrix A (2x3):" << mat1 << std::endl;
+    std::cout << "matrix B (3x2):" << mat2 << std::endl;
+    std::cout << "result matrix C (2x2) = AB:" << mat3 << std::endl;
+    std::cout << "result matrix C (2x2) From Naive = AB:" << mat3_naive << std::endl;
+    std::cout << "result matrix C (2x2) From MKL   = AB:" << mat3_mkl << std::endl;
+
+    // version from yungyuc
+    Matrix mat4 = mat2 * mat1;
+
+    // version from yanagi
+    Matrix mat4_naive = Matrix::multiply_naive(mat2, mat1);
+    Matrix mat4_mkl = Matrix::multiply_mkl(mat2, mat1);
+
+    std::cout << ">>> B(3x2) times A(2x3):" << std::endl;    
+    std::cout << "matrix B (3x2):" << mat2 << std::endl;
+    std::cout << "matrix A (2x3):" << mat1 << std::endl;
+    std::cout << "result matrix D (3x3) = BA:" << mat4 << std::endl;
+    std::cout << "result matrix D (3x3) From Naive = BA:" << mat4_naive << std::endl;
+    std::cout << "result matrix D (3x3) From MKL   = BA:" << mat4_mkl << std::endl;
+
+    return 0;
+}
---
+++ hw3/yanagiragi/matrix.cpp
@@ -0,0 +1,55 @@
+#include "matrix.hpp"
+
+std::ostream & operator<< (std::ostream & ostr, Matrix const & mat)
+{
+    for (size_t i=0; i<mat.nrow(); ++i)
+    {
+        ostr << std::endl << " ";
+        for (size_t j=0; j<mat.ncol(); ++j)
+        {
+            ostr << " " << std::setw(2) << mat(i, j);
+        }
+    }
+
+    return ostr;
+}
+
+Matrix operator*(Matrix const & mat1, Matrix const & mat2)
+{
+    if (mat1.ncol() != mat2.nrow())
+    {
+        throw std::out_of_range(
+            "the number of first matrix column "
+            "differs from that of second matrix row");
+    }
+
+    Matrix ret(mat1.nrow(), mat2.ncol());
+
+    for (size_t i=0; i<ret.nrow(); ++i)
+    {
+        for (size_t k=0; k<ret.ncol(); ++k)
+        {
+            double v = 0;
+            for (size_t j=0; j<mat1.ncol(); ++j)
+            {
+                v += mat1(i,j) * mat2(j,k);
+            }
+            ret(i,k) = v;
+        }
+    }
+
+    return ret;
+}
+
+
+// Self Implementation starts here
+
+Matrix Matrix::multiply_naive (const Matrix &left, const Matrix& right) 
+{
+    return left; // TODO
+}
+
+Matrix Matrix::multiply_mkl (const Matrix &left, const Matrix& right) 
+{
+    return left; // TODO
+}
