0fff56d87c5dc5ee18e50b6f1eb2633a4db38177
siméon <gaumart@etud.insa-toulouse.fr>
2020-04-28 13:11:06 +0200

    hw3 attempt1

---
+++ hw3/sgaumi/test_python.py
@@ -0,0 +1,122 @@
+import pytest
+import unittest
+import numpy.random as rd
+import time 
+from Matrix import Matrix, multiply_naive, multiply_mkl
+import numpy as np
+
+
+def multiply_naive_python(a,b):
+	res = Matrix(a.nrow,b.ncol)
+	for i in range(0,a.nrow):
+		for j in range(0,b.ncol):
+			s=0
+			for k in range(0,a.ncol):
+				s+=a[i,k]*b[k,j]
+			res[i,j]=s
+	return res
+
+
+################## TEST ##########################
+class test_python(unittest.TestCase):
+
+	def test_matrix(self):
+		row=rd.randint(1,1000)
+		col=rd.randint(1,1000)
+		test1=Matrix(row,col)
+		test2=Matrix(test1)
+		test3=Matrix(rd.random((row,col)))
+
+		self.assertEqual(test1.nrow,row)
+		self.assertEqual(test1.ncol,col)
+
+		self.assertEqual(test2.nrow,row)
+		self.assertEqual(test2.ncol,col)
+
+		self.assertEqual(test3.nrow,row)
+		self.assertEqual(test3.ncol,col)
+
+		self.assertEqual(test1,test2)
+		self.assertTrue(test1==test2)
+
+
+
+	def test_multiplynaive(self):
+		r=rd.randint(1,100)
+		c=rd.randint(1,100)
+		k=rd.randint(1,100)
+		m1=Matrix(rd.random((r,k)))
+		m2=Matrix(rd.random((k,c)))
+		mulc=multiply_naive(m1,m2)
+		mulp=multiply_naive_python(m1,m2)
+
+		self.assertEqual(mulc.nrow,r)
+		self.assertEqual(mulc.ncol,c)
+
+		self.assertEqual(mulc,mulp)
+		
+
+	def test_multiplymkl(self):
+		r=rd.randint(1,100)
+		c=rd.randint(1,100)
+		k=rd.randint(1,100)
+		m1=Matrix(rd.random((r,k)))
+		m2=Matrix(rd.random((k,c)))
+		mulc=multiply_mkl(m1,m2)
+		mulp=multiply_naive_python(m1,m2)
+
+		self.assertEqual(mulc.nrow,r)
+		self.assertEqual(mulc.ncol,c)
+
+		for i in range(0,r):
+			for j in range(0,c):
+				self.assertTrue(np.allclose(mulc[i,j],mulp[i,j]))
+
+
+
+	def test_performancemul(self):
+		r=rd.randint(100,150)
+		c=rd.randint(100,150)
+		k=rd.randint(100,150)
+		m1=Matrix(rd.random((r,k)))
+		m2=Matrix(rd.random((k,c)))
+		timer=[]
+		i=0
+
+		timer_start=time.time()
+		mulc_naive=multiply_naive(m1,m2)
+		timer_stop=time.time()
+		timer.append(timer_stop-timer_start)
+		i_mulcnaive=i
+		i+=1
+
+		timer_start=time.time()
+		mulp_naive=multiply_naive_python(m1,m2)
+		timer_stop=time.time()
+		timer.append(timer_stop-timer_start)
+		i_mulpnaive=i
+		i+=1
+
+		timer_start=time.time()
+		mulc_mkl=multiply_mkl(m1,m2)
+		timer_stop=time.time()
+		timer.append(timer_stop-timer_start)
+		i_mulcmkl=i
+		i+=1
+
+		with open('performance.txt','w') as fil:
+			fil.write("matrix 1 : {} rows {} columns\n".format(r,k))
+			fil.write("matrix 2 : {} rows {} columns\n".format(k,c))
+			fil.write("Performance test for matrix multiplication:\n")
+			fil.write("multiply_naive : {} seconds\n".format(timer[i_mulcnaive]))
+			fil.write("multiply_mkl : {} seconds\n".format(timer[i_mulcmkl]))
+			fil.write("( multiply_naive_python : {} seconds )\n".format(timer[i_mulpnaive]))
+			fil.close()		
+
+		self.assertTrue(True)		
+
+
+
+
+
+
