177346e6afe86cf881a67f611ab3ee7ffaae23d6
Ziv <zhongturtle@gmail.com>
2020-05-03 15:39:43 +0800

    Add files via upload

--- hw3/zivzone/performance.py
+++ hw3/zivzone/performance.py
@@ -1,14 +1,6 @@
 import time
 import _matrix
 
-
-# def bench(multiply, ma, mb):
-#     start = time.time()
-#     multiply(ma, mb)
-#     end = time.time()
-#     return end - start
-
-
 def main():
     first_matrix = _matrix.Matrix(1100, 1100)
     for i in range(1100):
@@ -18,21 +10,15 @@ def main():
     second_matrix = _matrix.Matrix(1100, 1100)
     for i in range(1100):
         for j in range(1100):
-            second_matrix[i, j] = 1
-
-    #tm = bench(_matrix.multiply_mkl, ma, mb)
-    
+            second_matrix[i, j] = 1    
     start_1 = time.time()
     _matrix.multiply_mkl(first_matrix, second_matrix)
-    #multiply(ma, mb)
     end_1 = time.time()
-    #return end - start
-    print('multiply_mkl', end_1 - start_1)
+
     start_2 = time.time()
-    #tn = bench(_matrix.multiply_naive, ma, mb)
     _matrix.multiply_naive(first_matrix,second_matrix)
     end_2 = time.time()
-    print('multiply_naive', end_2 - start_2)
+
     state_1 = "multiply_mkl use:" + str(end_1 - start_1)
     state_2 = "multiply_naive use:" + str(end_2 - start_2)
 
