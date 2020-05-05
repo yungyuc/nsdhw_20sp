9b82b73ad3beb4d68d3078a178b125c4127c8c23
Wilbert <wilbert.phen@gmail.com>
2020-05-05 18:29:58 +0800

    Reduce unused line to improve timing

--- hw4/WarClans612/_matrix.cpp
+++ hw4/WarClans612/_matrix.cpp
@@ -16,8 +16,7 @@ class Matrix {
 public:
 
     Matrix(size_t nrow, size_t ncol);
-    Matrix(size_t nrow, size_t ncol, int lsize);
-    Matrix(Matrix const & other);
+    Matrix(Matrix const & other, int x_pad, int y_pad);
     Matrix(Matrix && other);
     Matrix(std::vector<std::vector<double>> const & other);
 
@@ -28,9 +27,11 @@ public:
     friend Matrix multiply_mkl(const Matrix &mat1, const Matrix &mat2);
     friend Matrix multiply_tile(const Matrix &mat1, const Matrix &mat2, int lsize);
 
-    size_t index(size_t row, size_t col) const { return row*m_ncol + col; }
-    double   operator() (size_t row, size_t col) const { if (row >= m_nrow || col >= m_ncol) return 0; else return m_buffer.at( index(row, col) ); }
-    double & operator() (size_t row, size_t col)       { if (row >= m_nrow || col >= m_ncol) return trash; else return m_buffer.at( index(row, col) ); }
+    double   operator() (size_t row, size_t col) const { return m_buffer.at( row*m_ncol + col ); }
+    double & operator() (size_t row, size_t col)       { return m_buffer.at( row*m_ncol + col ); }
+    double   operator() (size_t idx) const { return m_buffer.at( idx ); }
+    double & operator() (size_t idx)       { return m_buffer.at( idx ); }
+    void unpad(int x_pad, int y_pad);
 
     bool operator== (Matrix const &other)
     {
@@ -41,17 +42,13 @@ public:
     }
 
     double *data() { return m_buffer.data(); }
-    size_t real_row() const { return m_nrow; }
-    size_t real_col() const { return m_ncol; }
-    int nrow() const { return ((m_nrow/tsize) + (m_nrow%tsize != 0))*tsize; }
-    int ncol() const { return ((m_ncol/tsize) + (m_ncol%tsize != 0))*tsize; }
-    int tsize = 1;
+    size_t nrow() const { return m_nrow; }
+    size_t ncol() const { return m_ncol; }
 
 private:
 
     size_t m_nrow;
     size_t m_ncol;
-    double trash;
     std::vector<double> m_buffer;
 
 };
@@ -63,18 +60,18 @@ Matrix::Matrix(size_t nrow, size_t ncol)
     std::fill(m_buffer.begin(), m_buffer.end(), 0);
 }
 
-Matrix::Matrix(size_t nrow, size_t ncol, int lsize)
-    : m_nrow(nrow), m_ncol(ncol), m_buffer(nrow * ncol, 0)
-{
-    std::fill(m_buffer.begin(), m_buffer.end(), 0);
-    tsize = lsize;
-}
-
 // copy constructor
-Matrix::Matrix(Matrix const & other)
-    : m_nrow(other.m_nrow), m_ncol(other.m_ncol), m_buffer(other.m_nrow * other.m_ncol, 0)
+Matrix::Matrix(Matrix const & other, int x_pad=0, int y_pad=0)
+    : m_nrow(other.m_nrow+x_pad), m_ncol(other.m_ncol+y_pad), m_buffer((other.m_nrow+x_pad) * (other.m_ncol+y_pad), 0)
 {
-    std::copy(other.m_buffer.begin(), other.m_buffer.end(), m_buffer.begin());
+    for(size_t i=0; i < m_nrow; ++i)
+    {
+        const size_t base_t = i*m_ncol;
+        const size_t base_s = i*other.m_ncol;
+        for (size_t j=0; j < m_ncol; ++j)
+            if (i >= other.m_nrow || j >= other.m_ncol) m_buffer.at(base_t + j) = 0;
+            else m_buffer.at(base_t + j) = other.m_buffer.at(base_s + j);
+    }
 }
 
 // move constructor
@@ -91,6 +88,25 @@ Matrix::Matrix(std::vector<std::vector<double>> const & other)
         m_buffer.insert(m_buffer.end(), v.begin(), v.end()); 
 }
 
+void Matrix::unpad(int x_pad, int y_pad)
+{
+    size_t x = m_nrow-x_pad;
+    size_t y = m_ncol-y_pad;
+    std::vector<double> temp(x*y);
+    for(size_t i=0; i < x; ++i)
+    {
+        const size_t base_t = i*y;
+        const size_t base_s = i*m_ncol;
+        for (size_t j=0; j < y; ++j)
+            temp.at(base_t + j) = m_buffer.at(base_s + j);
+    }
+    m_buffer.resize(x*y);
+    m_buffer.swap(temp);
+    m_nrow = x;
+    m_ncol = y;
+}
+
+
 class Block {
 
 public:
@@ -99,9 +115,10 @@ public:
 
     ~Block() = default; // default destructor
 
-    size_t index(size_t row, size_t col) const { return row*NDIM + col; }
-    double   operator() (size_t row, size_t col) const { if (row >= NDIM || col >= NDIM) return 0; else return m_buffer.at( index(row, col) ); }
-    double & operator() (size_t row, size_t col)       { if (row >= NDIM || col >= NDIM) return trash; else return m_buffer.at( index(row, col) ); }
+    double   operator() (size_t row, size_t col) const { return m_buffer.at( row*NDIM + col ); }
+    double & operator() (size_t row, size_t col)       { return m_buffer.at( row*NDIM + col ); }
+    double   operator() (size_t idx) const { return m_buffer.at( idx ); }
+    double & operator() (size_t idx)       { return m_buffer.at( idx ); }
 
     Block &operator= (double v);
     Block &operator+= (Block const &other);
@@ -111,7 +128,6 @@ public:
 private:
 
     size_t NDIM;
-    double trash;
     std::vector<double> m_buffer;
 };
 
@@ -124,32 +140,31 @@ Block::Block(size_t N)
 
 Block& Block::operator= (double v)
 {
-    for (size_t i=0; i<NDIM*NDIM; ++i) { m_buffer.at(i) = v; }
+    std::fill(m_buffer.begin(), m_buffer.end(), v);
     return *this;
 }
 
 Block& Block::operator+= (Block const &other)
 {
-    for (size_t i=0; i<NDIM*NDIM; ++i) { m_buffer.at(i) += other.m_buffer.at(i); }
+    const size_t max_i = NDIM*NDIM;
+    for (size_t i=0; i<max_i; ++i) { m_buffer.at(i) += other.m_buffer.at(i); }
     return *this;
 }
 
 void Block::save(Matrix &mat, size_t it, size_t jt)
 {
     const size_t ncol = mat.ncol();
+    const size_t a = it*NDIM;
+    const size_t b = jt*NDIM;
 
     for (size_t i=0; i<NDIM; ++i)
     {
         const size_t base_s = i*NDIM;
-        const size_t base_t = (it*NDIM + i) * ncol + jt*NDIM;
+        const size_t base_t = (a + i)*ncol + b;
 
         for (size_t j=0; j<NDIM; ++j)
         {
-            size_t x1 = (base_t + j) / mat.ncol();
-            size_t y1 = (base_t + j) % mat.ncol();
-            size_t x2 = (base_s + j) / NDIM;
-            size_t y2 = (base_s + j) % NDIM;
-            mat(x1, y1) += (*this)(x2, y2);
+            mat(base_t + j) += (*this)(base_s + j);
         }
     }
 }
@@ -191,51 +206,25 @@ void Tiler::load(
     Matrix const & mat2, size_t it2, size_t jt2
 )
 {
+    //const size_t nrow1 = mat1.nrow();
     const size_t ncol1 = mat1.ncol();
-
-    for (size_t i=0; i<NDIM; ++i)
-    {
-        const size_t base_t = i*NDIM;
-        const size_t base_s = (it1*NDIM + i) * ncol1 + jt1*NDIM;
-
-        for (size_t j=0; j<NDIM; ++j)
-        {
-            size_t x1 = (base_t + j) / NDIM;
-            size_t y1 = (base_t + j) % NDIM;
-            size_t x2 = (base_s + j) / mat1.ncol();
-            size_t y2 = (base_s + j) % mat1.ncol();
-            m_mat1(x1, y1) = mat1(x2, y2);
-        }
-    }
-
+    const size_t a1 = it1*NDIM;
+    const size_t b1 = jt1*NDIM;
+    //const size_t nrow2 = mat2.nrow();
     const size_t ncol2 = mat2.ncol();
+    const size_t a2 = it2*NDIM;
+    const size_t b2 = jt2*NDIM;
 
     for (size_t i=0; i<NDIM; ++i)
     {
-        const size_t base_t = i*NDIM;
-        const size_t base_s = (it2*NDIM + i) * ncol2 + jt2*NDIM;
+        const size_t base_s1 = (a1 + i)*ncol1 + b1;
+        const size_t base_s2 = (a2 + i)*ncol2 + b2;
+        const size_t base_t1 = i*NDIM;
 
-        for (size_t j=0; j<NDIM; ++j)
+        for (size_t j=0, base_t2=0; j<NDIM; ++j, base_t2+=NDIM)
         {
-            size_t x1 = (base_t + j) / NDIM;
-            size_t y1 = (base_t + j) % NDIM;
-            size_t x2 = (base_s + j) / mat2.ncol();
-            size_t y2 = (base_s + j) % mat2.ncol();
-            m_ret(x1, y1) = mat2(x2, y2);
-        }
-    }
-
-    for (size_t i=0; i<NDIM; ++i)
-    {
-        const size_t base = i*NDIM;
-
-        for (size_t j=0; j<NDIM; ++j)
-        {
-            size_t x1 = (j*NDIM + i) / NDIM;
-            size_t y1 = (j*NDIM + i) % NDIM;
-            size_t x2 = (base + j) / NDIM;
-            size_t y2 = (base + j) % NDIM;
-            m_mat2(x1, y1) = m_ret(x2, y2);
+            m_mat1(base_t1 + j) = mat1(base_s1 + j);
+            m_mat2(base_t2 + i) = mat2(base_s2 + j);
         }
     }
 }
@@ -244,24 +233,14 @@ void Tiler::multiply()
 {
     for (size_t i=0; i<NDIM; ++i)
     {
-        const size_t base1 = i*NDIM;
-
         for (size_t k=0; k<NDIM; ++k)
         {
-            const size_t base2 = k*NDIM;
-
             double v = 0;
             for (size_t j=0; j<NDIM; ++j)
             {
-                size_t x1 = (base1 + j) / NDIM;
-                size_t y1 = (base1 + j) % NDIM;
-                size_t x2 = (base2 + j) / NDIM;
-                size_t y2 = (base2 + j) % NDIM;
-                v += m_mat1(x1, y1) * m_mat2(x2, y2);
+                v += m_mat1(i, j) * m_mat2(k, j);
             }
-            size_t x1 = (base1 + k) / NDIM;
-            size_t y1 = (base1 + k) % NDIM;
-            m_ret(x1, y1) = v;
+            m_ret(i, k) = v;
         }
     }
 }
@@ -317,24 +296,22 @@ Matrix multiply_mkl(const Matrix &mat1, const Matrix &mat2)
 
 Matrix multiply_tile(const Matrix &m1, const Matrix &m2, int lsize)
 {
-
     validate_multiplication(m1, m2);
 
-    // New matrix to be returned
-    Matrix ret(m1.m_nrow, m2.m_ncol);
-    Matrix mat1(m1);
-    Matrix mat2(m2);
-    mat1.tsize = lsize;
-    mat2.tsize = lsize;
+    int nx1 = ((m1.nrow()/lsize) + (m1.nrow()%lsize != 0)) * lsize - m1.nrow();
+    int ny1 = ((m1.ncol()/lsize) + (m1.ncol()%lsize != 0)) * lsize - m1.ncol();
+    int nx2 = ((m2.nrow()/lsize) + (m2.nrow()%lsize != 0)) * lsize - m2.nrow();
+    int ny2 = ((m2.ncol()/lsize) + (m2.ncol()%lsize != 0)) * lsize - m2.ncol();
+    Matrix mat1(m1, nx1, ny1);
+    Matrix mat2(m2, nx2, ny2);
 
     const size_t nrow1 = mat1.nrow();
     const size_t ncol1 = mat1.ncol();
     //const size_t nrow2 = mat2.nrow();
     const size_t ncol2 = mat2.ncol();
 
-    std::cout << nrow1 << " " << m1.m_nrow << std::endl;
-    std::cout << ncol1 << " " << m1.m_ncol << std::endl;
-    std::cout << ncol2 << " " << m2.m_ncol << std::endl;
+    // New matrix to be returned
+    Matrix ret(nrow1, ncol2);
 
     const size_t ntrow1 = nrow1 / lsize;
     const size_t ntcol1 = ncol1 / lsize;
@@ -348,6 +325,7 @@ Matrix multiply_tile(const Matrix &m1, const Matrix &m2, int lsize)
     {
         for (size_t kt=0; kt<ntcol2; ++kt)
         {
+
             value = 0;
             for (size_t jt=0; jt<ntcol1; ++jt)
             {
@@ -359,6 +337,7 @@ Matrix multiply_tile(const Matrix &m1, const Matrix &m2, int lsize)
         }
     }
 
+    ret.unpad(nx1, ny2);
     return ret;
 }
 
@@ -368,18 +347,18 @@ PYBIND11_MODULE(_matrix, m) {
     m.def("multiply_tile", &multiply_tile);
     py::class_<Matrix>(m, "Matrix", py::buffer_protocol())
         .def(py::init<size_t, size_t>())
-        .def(py::init<Matrix const &>())
+        //.def(py::init<Matrix const &>())
         .def(py::init<std::vector<std::vector<double>>&>())
-        .def_property("nrow", &Matrix::real_row, nullptr)
-        .def_property("ncol", &Matrix::real_col, nullptr)
+        .def_property("nrow", &Matrix::nrow, nullptr)
+        .def_property("ncol", &Matrix::ncol, nullptr)
         .def_buffer([] (Matrix &m) -> py::buffer_info {
             return py::buffer_info(
                 m.data(),
                 sizeof(double),
                 py::format_descriptor<double>::format(),
                 2,
-                { m.real_row(), m.real_col() },
-                { sizeof(double) * m.real_col(), sizeof(double)}
+                { m.nrow(), m.ncol() },
+                { sizeof(double) * m.ncol(), sizeof(double)}
             );
         })
         .def("__eq__", &Matrix::operator==)
