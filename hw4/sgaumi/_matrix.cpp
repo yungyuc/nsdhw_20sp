6b53dfb747312c6f59a7b0b5c3013c209cad2092
siméon <gaumart@etud.insa-toulouse.fr>
2020-05-13 13:13:52 +0200

    hw4 v1 -> need to test on AWS

--- hw4/sgaumi/_matrix.cpp
+++ hw4/sgaumi/_matrix.cpp
@@ -97,7 +97,7 @@ public:
 	double *buff() { return m_buffer; }
 
 
-private:
+public:
 
 	void reset_buffer(size_t nrow, size_t ncol)
     {
@@ -159,20 +159,228 @@ Matrix multiply_mkl(Matrix const& m1, Matrix const& m2)
     return m;
 }
 
+/////////////HW4///////////////////////
+
+template<size_t N>
+struct Block
+{
+    static constexpr const size_t NDIM = N;
+
+    double   operator[] (size_t idx) const { return m_buffer[idx]; }
+    double & operator[] (size_t idx)       { return m_buffer[idx]; }
+
+    Block<N> & operator= (double v)
+    {
+        for (size_t i=0; i<N*N; ++i) { m_buffer[i] = v; }
+        return *this;
+    }
+
+    Block<N> & operator+= (Block<N> const & other)
+    {
+        for (size_t i=0; i<N*N; ++i) { m_buffer[i] += other.m_buffer[i]; }
+        return *this;
+    }
+
+    void save(Matrix & mat, size_t it, size_t jt);
+
+    double m_buffer[N * N];
+};
+
+template<size_t N> void Block<N>::save(
+    Matrix & mat, size_t it, size_t jt
+)
+{
+    const size_t ncol = mat.ncol();
+
+    for (size_t i=0; i<NDIM; ++i)
+    {
+        const size_t base_s = i*NDIM;
+        const size_t base_t = (it*NDIM + i) * ncol + jt*NDIM;
+
+        for (size_t j=0; j<NDIM; ++j)
+        {
+            mat.m_buffer[base_t + j] = m_buffer[base_s + j];
+        }
+    }
+}
+
+template<size_t N>
+struct Tiler
+{
+    static constexpr const size_t NDIM = N;
+
+    void load(
+        Matrix const & mat1, size_t it1, size_t jt1
+      , Matrix const & mat2, size_t it2, size_t jt2
+    );
+
+    void multiply();
+
+    Block<N> m_mat1; // row-major
+    Block<N> m_mat2; // column-major
+    Block<N> m_ret; // row-major
+};
+
+template<size_t N> void Tiler<N>::load(
+    Matrix const & mat1, size_t it1, size_t jt1
+  , Matrix const & mat2, size_t it2, size_t jt2
+)
+{
+    const size_t ncol1 = mat1.ncol();
+
+    for (size_t i=0; i<NDIM; ++i)
+    {
+        const size_t base_t = i*NDIM;
+        const size_t base_s = (it1*NDIM + i) * ncol1 + jt1*NDIM;
+
+        for (size_t j=0; j<NDIM; ++j)
+        {
+            m_mat1[base_t + j] = mat1.m_buffer[base_s + j];
+        }
+    }
+
+    const size_t ncol2 = mat2.ncol();
+
+    for (size_t i=0; i<NDIM; ++i)
+    {
+        const size_t base_t = i*NDIM;
+        const size_t base_s = (it2*NDIM + i) * ncol2 + jt2*NDIM;
+
+        for (size_t j=0; j<NDIM; ++j)
+        {
+            m_ret[base_t + j] = mat2.m_buffer[base_s + j];
+        }
+    }
+
+    for (size_t i=0; i<NDIM; ++i)
+    {
+        const size_t base = i*NDIM;
+
+        for (size_t j=0; j<NDIM; ++j)
+        {
+            m_mat2[j*NDIM + i] = m_ret[base + j];
+        }
+    }
+}
+
+template<size_t N> void Tiler<N>::multiply()
+{
+    for (size_t i=0; i<NDIM; ++i)
+    {
+        const size_t base1 = i*NDIM;
+
+        for (size_t k=0; k<NDIM; ++k)
+        {
+            const size_t base2 = k*NDIM;
+
+            double v = 0;
+            for (size_t j=0; j<NDIM; ++j)
+            {
+                v += m_mat1[base1 + j] * m_mat2[base2 + j];
+            }
+            m_ret[base1 + k] = v;
+        }
+    }
+}
+
+//util//
+Matrix copy_fill0(Matrix const& m,size_t r,size_t c){
+	
+	Matrix ret(m.nrow()+r,m.ncol()+c);
+	for(size_t i=0;i<m.nrow();i++){
+		for(size_t j=0;j<m.ncol();j++){
+			ret(i,j)=m(i,j);
+		}
+	}
+	for(size_t i=0;i<r;i++){
+		for(size_t j=0;j<c;j++){
+			ret(m.nrow()+i,m.ncol()+j)=0;
+		}
+	}
+	return ret;
+}
+
+Matrix suppr0(Matrix const& m,size_t r,size_t c){
+	
+	Matrix ret(m.nrow()-r,m.ncol()-c);
+	for(size_t i=0;i<m.nrow()-r;i++){
+		for(size_t j=0;j<m.ncol()-c;j++){
+			ret(i,j)=m(i,j);
+		}
+	}
+	return ret;
+}
+////
+
+
+//Tiled matrix matrix multiplication.
+Matrix multiply_tile(Matrix const & mat1_0, Matrix const & mat2_0)
+{
+
+    constexpr const size_t tsize = 64 / sizeof(double);
+
+	size_t r1 = tsize - (mat1_0.nrow()%tsize);
+	size_t c1= tsize - (mat1_0.ncol()%tsize);
+	size_t r2 = tsize - (mat2_0.nrow()%tsize);
+	size_t c2= tsize - (mat2_0.ncol()%tsize);
+
+	//work with matrix where nrow ncol are divider of tsize
+	Matrix mat1 = copy_fill0(mat1_0,r1,c1);
+	Matrix mat2 = copy_fill0(mat2_0,r2,c2);
+
+	Matrix ret(mat1.nrow(), mat2.ncol());
+
+    const size_t nrow1 = mat1.nrow();
+    const size_t ncol1 = mat1.ncol();
+    const size_t ncol2 = mat2.ncol();
+
+    const size_t ntrow1 = nrow1 / tsize;
+    const size_t ntcol1 = ncol1 / tsize;
+    const size_t ntcol2 = ncol2 / tsize;
+
+    Block<tsize> value;
+    Tiler<tsize> tiler;
+
+    for (size_t it=0; it<ntrow1; ++it)
+    {
+        for (size_t kt=0; kt<ntcol2; ++kt)
+        {
+            value = 0;
+            for (size_t jt=0; jt<ntcol1; ++jt)
+            {
+                tiler.load(mat1, it, jt, mat2, jt, kt);
+                tiler.multiply();
+                value += tiler.m_ret;
+            }
+            value.save(ret, it, kt);
+        }
+    }
+
+	Matrix ret_0 = suppr0(ret,r1,c2);
+    return ret_0;
+}
+
+       
+////////////////HW4(end)///////////////////////
+
+
+
+
 PYBIND11_MODULE(_matrix, m){
     m.def("multiply_naive", &multiply_naive);
     m.def("multiply_mkl", &multiply_mkl);
+	m.def("multiply_tile", &multiply_tile);
 
     py::class_<Matrix>(m, "Matrix", py::buffer_protocol())
-        .def(py::init([](size_t r, size_t c) {return new Matrix(r, c);}))
+        .def(py::init([](size_t r, size_t c) {
+			return new Matrix(r, c);
+		}))
         .def(py::init<Matrix&>())
         .def(py::init<std::vector<std::vector<double>>&>())
         .def_property("nrow", &Matrix::nrow, nullptr)
         .def_property("ncol", &Matrix::ncol, nullptr)
         .def("__eq__", &Matrix::operator==)
         //.def("__repr__", )
-
-
 		.def("__setitem__", [](Matrix &m, std::pair<size_t,size_t> p, double val){
             m(p.first, p.second)= val;
         })
--- hw4/sgaumi/test_python.py
+++ hw4/sgaumi/test_python.py
@@ -2,7 +2,7 @@ import pytest
 import unittest
 import numpy.random as rd
 import time 
-from _matrix import Matrix, multiply_naive, multiply_mkl
+from _matrix import Matrix, multiply_naive, multiply_tile, multiply_mkl
 import numpy as np
 
 
@@ -73,6 +73,24 @@ class test_python(unittest.TestCase):
 				self.assertTrue(np.allclose(mulc[i,j],mulp[i,j]))
 
 
+	def test_multiplytile(self):
+		r=rd.randint(1,100)
+		c=rd.randint(1,100)
+		k=rd.randint(1,100)
+		m1=Matrix(rd.random((r,k)))
+		m2=Matrix(rd.random((k,c)))
+		mulc=multiply_tile(m1,m2)
+		mulp=multiply_naive_python(m1,m2)
+
+		self.assertEqual(mulc.nrow,r)
+		self.assertEqual(mulc.ncol,c)
+
+		for i in range(0,r):
+			for j in range(0,c):
+				print("i={},j={} : tile : {} == naive : {}".format(i,j,mulc[i,j],mulp[i,j]))
+				self.assertTrue(np.allclose(mulc[i,j],mulp[i,j]))
+
+
 
 	def test_performancemul(self):
 		r=rd.randint(1000,1500)
@@ -104,16 +122,27 @@ class test_python(unittest.TestCase):
 		i_mulcmkl=i
 		i+=1
 
+		timer_start=time.time()
+		mulc_mkl=multiply_tile(m1,m2)
+		timer_stop=time.time()
+		timer.append(timer_stop-timer_start)
+		i_mulctile=i
+		i+=1
+
+		runtime=timer[i_mulctile]/timer[i_mulcnaive]*100.
+		speedup=(1.-timer[i_mulctile]/timer[i_mulcnaive])*100.
 		with open('performance.txt','w') as fil:
 			fil.write("matrix 1 : {} rows {} columns\n".format(r,k))
 			fil.write("matrix 2 : {} rows {} columns\n".format(k,c))
 			fil.write("Performance test for matrix multiplication:\n")
 			fil.write("multiply_naive : {} seconds\n".format(timer[i_mulcnaive]))
 			fil.write("multiply_mkl : {} seconds\n".format(timer[i_mulcmkl]))
+			fil.write("multiply_tile : {} seconds\n".format(timer[i_mulctile]))
 			#fil.write("( multiply_naive_python : {} seconds )\n".format(timer[i_mulpnaive]))
+			fil.write("speedup of {}% from naive to tile multiplication\n".format(speedup))
+			fil.write("(runtime for tile multiplication is {}% of naive multiplication one)\n".format(runtime))
 			fil.close()		
-
-		self.assertTrue(True)		
+	
 
 
 
