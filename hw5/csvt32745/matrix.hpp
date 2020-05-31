e0b5fbd36c1f305ea97ce081f418c9fe49631b24
Alan Desktop <csvt32745@hotmail.com>
2020-05-31 23:02:07 +0800

    Rename Matrix src, Makefile fetch numpy lib

--- hw5/csvt32745/matrix.hpp
+++ hw5/csvt32745/matrix.hpp
@@ -122,7 +122,7 @@ class Matrix{
         return *this;
     }
 
-    bool operator==(Matrix const & other)
+    bool operator==(Matrix const & other) const
     {
         if (this == &other) { return true; }
         if (m_nrow != other.m_nrow || m_ncol != other.m_ncol)
@@ -324,34 +324,34 @@ Matrix multiply_tile(const Matrix& a, const Matrix& b, size_t tile_size)
     return ans;
 }
 
-PYBIND11_MODULE(_matrix, m){
-    m.def("multiply_naive", &multiply_naive);
-    m.def("multiply_mkl", &multiply_mkl);
-    m.def("multiply_tile", &multiply_tile);
+// PYBIND11_MODULE(_matrix, m){
+//     m.def("multiply_naive", &multiply_naive);
+//     m.def("multiply_mkl", &multiply_mkl);
+//     m.def("multiply_tile", &multiply_tile);
 
-    py::class_<Matrix>(m, "Matrix", py::buffer_protocol())
-        .def(py::init([](size_t r, size_t c) {return new Matrix(r, c);}))
-        .def(py::init<Matrix&>())
-        .def(py::init<std::vector<std::vector<double>>&>())
-        .def_property("nrow", &Matrix::nrow, nullptr)
-        .def_property("ncol", &Matrix::ncol, nullptr)
-        .def("__eq__", &Matrix::operator==)
-        .def("__repr__", &Matrix::reprString)
-        .def_buffer([](Matrix &m) -> py::buffer_info{
-            return py::buffer_info(
-                m.data(),
-                sizeof(double),
-                py::format_descriptor<double>::format(),
-                2,
-                { m.nrow(), m.ncol() },
-                { sizeof(double)*m.ncol(), sizeof(double) }
-            );
-        })
-        .def("__setitem__", [](Matrix &m, std::array<double, 2> tp, double val){
-            m.set_data(tp[0], tp[1], val);
-        })
-        .def("__getitem__", [](Matrix &m, std::array<double, 2> tp){
-            return m.get_data(tp[0], tp[1]);
-        })
-        ;
-}
+//     py::class_<Matrix>(m, "Matrix", py::buffer_protocol())
+//         .def(py::init([](size_t r, size_t c) {return new Matrix(r, c);}))
+//         .def(py::init<Matrix&>())
+//         .def(py::init<std::vector<std::vector<double>>&>())
+//         .def_property("nrow", &Matrix::nrow, nullptr)
+//         .def_property("ncol", &Matrix::ncol, nullptr)
+//         .def("__eq__", &Matrix::operator==)
+//         .def("__repr__", &Matrix::reprString)
+//         .def_buffer([](Matrix &m) -> py::buffer_info{
+//             return py::buffer_info(
+//                 m.data(),
+//                 sizeof(double),
+//                 py::format_descriptor<double>::format(),
+//                 2,
+//                 { m.nrow(), m.ncol() },
+//                 { sizeof(double)*m.ncol(), sizeof(double) }
+//             );
+//         })
+//         .def("__setitem__", [](Matrix &m, std::array<double, 2> tp, double val){
+//             m.set_data(tp[0], tp[1], val);
+//         })
+//         .def("__getitem__", [](Matrix &m, std::array<double, 2> tp){
+//             return m.get_data(tp[0], tp[1]);
+//         })
+//         ;
+// }
