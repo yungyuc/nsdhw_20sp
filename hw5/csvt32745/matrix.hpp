79539a8c004f63e5b20cec5f35e99ea7381db1a3
Alan Desktop <csvt32745@hotmail.com>
2020-06-02 09:16:22 +0800

    Remove including pybind11/operators.h in Matrix lib

--- hw5/csvt32745/matrix.hpp
+++ hw5/csvt32745/matrix.hpp
@@ -4,8 +4,8 @@
 #include <sstream>
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
-#include "pybind11/operators.h"
 #include "pybind11/numpy.h"
+// #include "pybind11/operators.h"
 #include "mkl.h"
 
 namespace py = pybind11;
