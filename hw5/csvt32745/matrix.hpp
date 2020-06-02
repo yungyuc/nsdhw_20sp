c9b18b7214a80987a1e9da83a7793f65b64fb7f8
Alan Desktop <csvt32745@hotmail.com>
2020-06-02 09:18:51 +0800

    Clean pybind11 content in Matrix lib

--- hw5/csvt32745/matrix.hpp
+++ hw5/csvt32745/matrix.hpp
@@ -2,9 +2,9 @@
 #include <array>
 #include <vector>
 #include <sstream>
-#include "pybind11/pybind11.h"
-#include "pybind11/stl.h"
-#include "pybind11/numpy.h"
+// #include "pybind11/pybind11.h"
+// #include "pybind11/stl.h"
+// #include "pybind11/numpy.h"
 // #include "pybind11/operators.h"
 #include "mkl.h"
 
